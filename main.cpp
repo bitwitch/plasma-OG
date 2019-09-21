355bf7fd61fcfc1d5bba635ea60be43a35932353
bitwitch <shawwian@gmail.com>
2019-09-21 00:58:27 -0400

    pack rgb palette into ints

--- main.cpp
+++ main.cpp
@@ -20,7 +20,8 @@ public:
     u8 *plasma3; // 
     u8 *plasma4; // 
     u8 *plasma5; // 
-    u8 *palette;
+
+    int *palette;
 
     void precalculate_plasma(u8 *buffer1, u8 *buffer2, u8 *buffer3, u8 *buffer4, u8 *buffer5, int w, int h)
     {
@@ -46,7 +47,7 @@ public:
         plasma3 = new u8[WIDTH*2*HEIGHT*2];
         plasma4 = new u8[WIDTH*2*HEIGHT*2];
         plasma5 = new u8[WIDTH*2*HEIGHT*2];
-        palette = new u8[256*3];
+        palette = new int[256];
 
         precalculate_plasma(plasma1, plasma2, plasma3, plasma4, plasma4, WIDTH*2, HEIGHT*2);
         return true;    
@@ -72,17 +73,15 @@ public:
         int i_plasma5 = y5 * WIDTH * 2 + x5;
 
         // set color palette
-        u8 r, g, b;
+        int r, g, b;
         int i, dst = 0;
         for (i=0; i<256; ++i) 
         {
-            r = (u8)(32 + 31 * cos(i * PI / 128 + current_time/7));
-            g = (u8)(32 + 31 * sin(i * PI / 128 + current_time/6));
-            b = (u8)(32 - 31 * cos(i * PI / 128 + current_time/8));
+            r = (int)(32 + 31 * cos(i * PI / 128 + current_time/7));
+            g = (int)(32 + 31 * sin(i * PI / 128 + current_time/6));
+            b = (int)(32 - 31 * cos(i * PI / 128 + current_time/8));
 
-            palette[dst++] = r; 
-            palette[dst++] = g; 
-            palette[dst++] = b; 
+            palette[dst++] = (r << 16) | (g << 8) | b; 
         }
 
         // draw
@@ -94,9 +93,9 @@ public:
                 // buffer and draw whole buffer once? and is this possible
                 // in olcPixelEngine?
                 //Draw(x, y, olc::Pixel(color_val, color_val, color_val));
-                Draw(x, y, olc::Pixel(palette[color_val*3], 
-                                      palette[color_val*3+1], 
-                                      palette[color_val*3+2]));
+                Draw(x, y, olc::Pixel((palette[color_val] >> 16)& 0xFF, 
+                                      (palette[color_val] >> 8) & 0xFF, 
+                                      palette[color_val] & 0xFF));
 
                 ++i_plasma1;
                 ++i_plasma2;
